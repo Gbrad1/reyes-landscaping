e7c411fb1a1fbb2e1028797c259272035b2cc39d
Gbrad1 <gilesbradford27@gmail.com>
2026-04-09 20:17:29 -0400

    Adding new photos and changed the layout of the projects.

--- reyes-hardscaping.js
+++ reyes-hardscaping.js
@@ -1,18 +1,56 @@
 // Define your local photo sets
 const photoSets = {
   'patios': [
-    'images/patios/patio-1.JPG'
+    'images/patios/patio-1.JPG',
+    'images/patios/patio-2.JPG',
+    'images/patios/patio-3.JPG',
+    'images/patios/patio-4.JPG',
+    'images/patios/patio-5.JPG',
+    'images/patios/patio-6.JPG',
+    'images/patios/patio-7.JPG',
+    'images/patios/patio-8.JPG',
+    'images/patios/patio-9.JPG',
+    'images/patios/patio-10.JPG',
+    'images/patios/patio-11.JPG',
+    'images/patios/patio-12.JPG',
+    'images/patios/patio-13.JPG'
     ],
   'retaining-walls': [
     'images/retaining-walls/retaining_wall_1.JPG',
     'images/retaining-walls/retaining_wall_2.JPG',
     'images/retaining-walls/retaining_wall_3.JPG',
-    'images/retaining-walls/retaining_wall_4.JPG'
+    'images/retaining-walls/retaining_wall_4.JPG',
+    'images/retaining-walls/retaining_wall_5.JPG',
+    'images/retaining-walls/retaining_wall_6.JPG',
+    'images/retaining-walls/retaining_wall_7.JPG',
+    'images/retaining-walls/retaining_wall_8.JPG'
     ],
   'walkways': [
-    'images/walkways/walkway-1.JPG'
+    'images/walkways/walkway-1.JPG',
+    'images/walkways/walkway-2.JPG',
+    'images/walkways/walkway-3.JPG',
+    'images/walkways/walkway-4.JPG',
+    'images/walkways/walkway-5.JPG',
+    'images/walkways/walkway-6.JPG',
+    'images/walkways/walkway-7.JPG',
+    'images/walkways/walkway-8.JPG',
+    'images/walkways/walkway-9.JPG',
+    'images/walkways/walkway-10.JPG',
+    'images/walkways/walkway-11.JPG',
+    'images/walkways/walkway-12.JPG',
+    'images/walkways/walkway-13.JPG'
     ],
-  'featured-project': []
+  'featured-project': [
+    'images/featured-project/fp-1.JPG',
+    'images/featured-project/fp-2.JPG',
+    'images/featured-project/fp-3.JPG',
+    'images/featured-project/fp-4.JPG',
+    'images/featured-project/fp-5.JPG',
+    'images/featured-project/fp-6.JPG',
+    'images/featured-project/fp-7.JPG',
+    'images/featured-project/fp-8.JPG',
+    'images/featured-project/fp-9.JPG'
+  ]
 };
 
 let currentSet = [];
